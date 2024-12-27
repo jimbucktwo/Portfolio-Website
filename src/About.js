65c06607e41683d9916d1b486489cd7d70e9a2c2
jimbucktwo <jimmytraann@gmail.com>
2024-12-26 22:03:47 -0800

    finished about page

--- src/About.js
+++ src/About.js
@@ -2,8 +2,10 @@ import "./About.css";
 import { Header, Footer } from "./Home";
 import { useEffect } from "react";
 import resume from "./Resume.pdf";
+import peanut from "./images/peanut.jpg";
+import jimmy from "./images/jimmy.JPEG";
 
-export default function About() {
+function Bio() {
   useEffect(() => {
     const introElement = document.querySelector(".about");
     const observer = new IntersectionObserver(
@@ -29,38 +31,134 @@ export default function About() {
       }
     };
   }, []);
+
+
+  return (
+    <div className="about">
+      <div className="about-section">
+        <img src={jimmy} alt="Jimmy" className="jimmy"/>
+        <h2>Jimmy Tran</h2>
+        <p id="about-1">
+          Hi! I am currently enrolled at California State University, Long Beach
+          and am in the midst of pursuing my Bachelors in Computer Science.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Courses() {
+  useEffect(() => {
+    const introElement = document.querySelector(".courses");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          introElement.classList.add("fade-in");
+        } else {
+          introElement.classList.remove("fade-in");
+        }
+      },
+      {
+        threshold: 0.3, // Adjust this value as needed
+      }
+    );
+
+    if (introElement) {
+      observer.observe(introElement);
+    }
+
+    return () => {
+      if (introElement) {
+        observer.unobserve(introElement);
+      }
+    };
+  }, []);
+
+  return(
+    <div className="courses">
+      <div className="courses-section">
+        
+      <h2>Past/Future Courses</h2>
+      
+      <ul className="courses-list">
+        <li>Artificial Intelligence</li>
+        <li>Cybersecurity</li>
+        <li>Data Structures and Algorithms</li>
+        <li>Databases (Relational and NoSQL)</li>
+        <li>Machine Learning</li>
+        <li>Mobile App Development in Android</li>
+        <li>Networking</li>
+        <li>Object-Oriented Programming</li>
+        <li>Operating Systems</li>
+        <li>Systems Programming</li>
+        <li>Web Development</li>
+      </ul>
+      </div>
+    </div>
+  )
+}
+
+
+function Experience() {
+  useEffect(() => {
+    const introElement = document.querySelector(".experience");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          introElement.classList.add("fade-in");
+        } else {
+          introElement.classList.remove("fade-in");
+        }
+      },
+      {
+        threshold: 0.3, // Adjust this value as needed
+      }
+    );
+
+    if (introElement) {
+      observer.observe(introElement);
+    }
+
+    return () => {
+      if (introElement) {
+        observer.unobserve(introElement);
+      }
+    };
+  }, []);
+
+
+  return (
+    <div className="experience">
+      <div className="experience-section">
+        
+      <ul className="experience-list">
+        <li>MongoDB</li>
+        <li>React</li>
+        <li>GitHub</li>
+        <li>Android Studio</li>
+        <li>PostGres</li>
+        <li>Visual Studio Code</li>
+        <li>Google Cloud Platform</li>
+      </ul> 
+          
+      
+      <h2>Experience</h2>
+      </div>
+      <a href={resume} download="Resume (Jimmy Tran)" className="resume1">
+          Resume
+          </a>
+    </div>
+  )
+}
+
+
+export default function About() {
   return (
     <div>
       <Header />
-      <div className="about">
-        <div className="about-section">
-          <h2>Jimmy Tran</h2>
-          <p id="about-1">
-            {" "}
-            Hi, I am currently enrolled at California State University, Long
-            Beach and am in the midst of pursuing my Bachelors in Computer
-            Science. My goals are to develop my web skills with emphasis on
-            front-end aspects. Throughout my college degree, I have taken
-            relevant courses such as:
-          </p>
-          <ul id="courses">
-            <li>Data Structures and Algorithms</li>
-            <li>Databases (Relational and NoSQL)</li>
-            <li>Mobile App Development in Android</li>
-            <li>Web Development</li>
-          </ul>
-          <p id="about-2">
-            I also have experience working with various different software and
-            frameworks, such as ReactJS, NodeJS, Android Studio, MongoDB, and
-            GitHub to name a few. I am also curious to learn new technologies
-            since I have a knack at picking things up quick. Click below to
-            explore more about me!
-          </p>
-          <a href={resume} download="Resume (Jimmy Tran)">
-            Resume
-          </a>
-        </div>
-      </div>
+      <Bio />
+      <Courses />
+      <Experience />
       <Footer />
     </div>
   );
--- src/Work.js
+++ src/Work.js
@@ -1,39 +1,90 @@
 import './Work.css';
-import {Header} from './Home';
+import {Header, Footer} from './Home';
 import {useEffect} from 'react';
+import work from "./images/work.jpg";
+import whizara_logo from "./images/whizara_logo.jpg";
+
+
+function Workpage() {
+  useEffect(() => {
+    const introElement = document.querySelector(".work");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          introElement.classList.add("fade-in");
+        } else {
+          introElement.classList.remove("fade-in");
+        }
+      },
+      {
+        threshold: 0.3, // Adjust this value as needed
+      }
+    );
+
+    if (introElement) {
+      observer.observe(introElement);
+    }
+
+    return () => {
+      if (introElement) {
+        observer.unobserve(introElement);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="work">
+      <div className="work-section">
+      <h2>Work</h2>
+      <div className="img">
+      <img src={work} alt="Work" className="work-img"/>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+function Instructor(){
+  useEffect(() => {
+    const introElement = document.querySelector(".instructor");
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          introElement.classList.add("fade-in");
+        } else {
+          introElement.classList.remove("fade-in");
+        }
+      },
+      {
+        threshold: 0.3, // Adjust this value as needed
+      }
+    );
+
+    if (introElement) {
+      observer.observe(introElement);
+    }
+
+    return () => {
+      if (introElement) {
+        observer.unobserve(introElement);
+      }
+    };
+  }, []);
+
+    return(
+        <div className="instructor">
+          <h2>CS Instructor at Whizara</h2>
+          <img src={whizara_logo} alt="Whizara" className="whizara-logo"/>
+        </div>);
+}
 
 export default function Work() {
-    useEffect(() => {
-        const introElement = document.querySelector(".work");
-        const observer = new IntersectionObserver(
-          ([entry]) => {
-            if (entry.isIntersecting) {
-              introElement.classList.add("fade-in");
-            } else {
-              introElement.classList.remove("fade-in");
-            }
-          },
-          {
-            threshold: 0.3, // Adjust this value as needed
-          }
-        );
-    
-        if (introElement) {
-          observer.observe(introElement);
-        }
-    
-        return () => {
-          if (introElement) {
-            observer.unobserve(introElement);
-          }
-        };
-      }, []);
-
     return (
         <div>
         <Header/>
-        <div className="work">
-        </div>
+        <Workpage/>
+        <Instructor/>
+        <Footer/>
         </div>
     );
 }
